3402764925af4c031b4542c018672a164f8c1e0e
Rahul Srivastava <srivastavarahul088@gmail.com>
2022-07-06 01:38:55 +0530

    landingimage issue resolved

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -2,10 +2,10 @@ import 'package:first_helpers/screens/login_screen.dart';
 import 'package:first_helpers/screens/user/user_signup_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:first_helpers/utilities/imgtxtButton.dart';
+import 'package:gif/gif.dart';
+
 
 class LandingScreen extends StatelessWidget {
-  const LandingScreen({Key? key}) : super(key: key);
-
   static const routeName = 'landing-screen';
 
   @override
@@ -15,56 +15,74 @@ class LandingScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Container(
               child: Image.asset('images/logoimage.png'),
               width: 100,
             ),
             Container(
+              padding: EdgeInsets.symmetric(horizontal: 8),
               child: Image.asset('images/brandname.png'),
               height: 60,
             ),
             Container(
-              child: Image.asset('images/landing.gif'),
-              height: 390,
+              child: Gif(
+                image: AssetImage('images/landing.gif'),
+                fps: 30,
+                autostart: true,
+              ),
+              height: MediaQuery.of(context).size.width,
             ),
-            Expanded(
-              flex: 3,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: imgtxtButton(
-                      img: 'images/ambulance.gif',
-                      txt: 'PARAMEDIC',
-                      onPressed: () {},
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: imgtxtButton(
-                      img: 'images/doctor.gif',
-                      txt: 'DOCTOR',
-                      onPressed: () {},
-                    ),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.3,
-                    child: Hero(
-                      tag: 'user.gif',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.5),
+                    child: SizedBox(
+                      // width: MediaQuery.of(context).size.width * 0.3,
                       child: imgtxtButton(
-                        img: 'images/user.gif',
-                        txt: 'USER',
-                        onPressed: () {
-                          Navigator.pushNamed(
-                              context, UserSignupScreen.routeName);
-                        },
+                        img: 'images/ambulance.gif',
+                        txt: 'PARAMEDIC',
+                        onPressed: () {},
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.5),
+                    child: SizedBox(
+                      // width: MediaQuery.of(context).size.width * 0.3,
+                      child: imgtxtButton(
+                        img: 'images/doctor.gif',
+                        txt: 'DOCTOR',
+                        onPressed: () {},
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(2.5),
+                    child: SizedBox(
+                      // width: MediaQuery.of(context).size.width * 0.3,
+                      child: Hero(
+                        tag: 'user.gif',
+                        child: imgtxtButton(
+                          img: 'images/user.gif',
+                          txt: 'USER',
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, UserSignupScreen.routeName);
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
