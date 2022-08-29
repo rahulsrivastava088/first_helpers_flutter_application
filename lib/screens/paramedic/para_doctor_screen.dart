8538955464f8e36f1bb11761235ddd21de09467c
Rahul Srivastava <srivastavarahul088@gmail.com>
2022-08-30 00:13:05 +0530

    Call added

--- lib/screens/paramedic/para_doctor_screen.dart
+++ lib/screens/paramedic/para_doctor_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
+import 'package:first_helpers/utilities/constants.dart';
 
 class ParaDoctor extends StatelessWidget {
   const ParaDoctor({Key? key}) : super(key: key);
@@ -23,8 +25,7 @@ class ParaDoctor extends StatelessWidget {
         ),
       ),
       body: StreamBuilder(
-          stream:
-              FirebaseFirestore.instance.collection("docs").snapshots(),
+          stream: FirebaseFirestore.instance.collection("docs").snapshots(),
           builder:
               (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
             if (!snapshot.hasData) {
@@ -38,11 +39,82 @@ class ParaDoctor extends StatelessWidget {
                     itemBuilder: ((context, index) {
                       DocumentSnapshot data = snapshot.data!.docs[index];
 
-                      return ChatTile(
-                          username: data['name'],
-                          field: data['specialization'],
-                          imageUrl:
-                              "https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?w=2000");
+                      // return ChatTile(
+                      //     username: data['name'],
+                      //     field: data['specialization'],
+                      //     imageUrl:
+                      //         "https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?w=2000");
+
+                      return Padding(
+                        padding: EdgeInsets.only(top: 8.0),
+                        child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20.0),
+                              side: new BorderSide(color: logoBlue, width: 2.0),
+                            ),
+                            margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 15.0, bottom: 5.0, left: 15, right: 5),
+                              child: Column(
+                                  // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        CircleAvatar(
+                                          radius: 25,
+                                          backgroundImage:
+                                              Image.network("https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?w=2000").image,
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            data['name'],
+                                            style: TextStyle(
+                                                fontSize: 17,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        data['specialization'],
+                                      ),
+                                    ),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.end,
+                                      children: [
+                                        MaterialButton(
+                                          onPressed: () async {
+                                            print("Working");
+                                            bool? res = await FlutterPhoneDirectCaller.callNumber(data['phoneNumber']);
+                                          },
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.end,
+                                            children: [
+                                              Icon(
+                                                Icons.call_sharp,
+                                                size: 20,
+                                              ),
+                                              Text(
+                                                "Call",
+                                                style: TextStyle(fontSize: 15),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ]),
+                            )),
+                      );
                     }),
                   ),
                 ),
---
+++ lib/utilities/callCardTile.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:first_helpers/screens/chatScreen.dart';
+
+import 'constants.dart';
+
+class CallTile extends StatelessWidget {
+  String username;
+  String imageUrl;
+  String field;
+  CallTile({required this.username, required this.imageUrl, required this.field});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            side: new BorderSide(color: logoBlue, width: 2.0),
+          ),
+          margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+          child: Padding(
+            padding: const EdgeInsets.only(
+                top: 15.0, bottom: 5.0, left: 15, right: 5),
+            child: Column(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CircleAvatar(
+                        radius: 25,
+                        backgroundImage: Image.network(imageUrl).image,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          username,
+                          style: TextStyle(
+                              fontSize: 17, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      field,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      MaterialButton(
+                        onPressed: () {
+                          print("Working");
+                          Navigator.pushNamed(context, ChatScreen.routeName);
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.message_rounded,
+                              size: 20,
+                            ),
+                            Text(
+                              "Start Chat!",
+                              style: TextStyle(fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+          )),
+    );
+  }
+}
